70b3f6ab349e5f86fde4e17c47a8a59eb5e20684
Nasser <nasserbrid@gmail.com>
2025-03-03 14:46:31 +0100

    ajout de dépendances gunicorn, dj_database_url et psycopg2 pour la base postgresql qu'il faudra créer dans render afin de déployer le backend

--- portfolioapi/portfolioapi/settings.py
+++ portfolioapi/portfolioapi/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+# import dj_database_url
+# import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -101,6 +103,16 @@ DATABASES = {
         },
     }
 }
+
+#configuration pour déployer sur render.com
+# DATABASE_URL = ""
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default = DATABASE_URL,
+#         engine = 'django.db.backends.postgresql'
+#     )
+# }
 
 
 # Password validation
