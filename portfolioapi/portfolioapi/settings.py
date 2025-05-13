a1ddae098543b9eb7df8438dafdb660acbb096d1
Nasser <nasserbrid@gmail.com>
2025-05-13 17:05:37 +0200

    maj DEBUG a FALSE

--- portfolioapi/portfolioapi/settings.py
+++ portfolioapi/portfolioapi/settings.py
@@ -33,18 +33,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = False
 
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if DEBUG:
-    ALLOWED_HOSTS = ['*']
-else:
-    ALLOWED_HOSTS = ['portfolio-backend-w2tk.onrender.com']
+# if DEBUG:
+#     ALLOWED_HOSTS = ['*']
+# else:
+#     ALLOWED_HOSTS = ['portfolio-backend-w2tk.onrender.com']
 
 
-# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
